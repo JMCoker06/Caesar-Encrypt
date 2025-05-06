f2e961186cb998c0b9ff74de41cec113f2e6f16c
Joseph Coker <JMCoker746@gmail.com>
2025-05-06 05:56:28 -0700

    This is my first commit to Caesar Encrypt

--- mycipher.cpp
+++ mycipher.cpp
@@ -1,25 +1,47 @@
 #include <iostream> //importing my library
 #include <string> //importing string library
 using namespace std; //allows me to use my library
-string Encrypt(string phrase, int shift) //creating and defining my Encrypt function
+string Input(const string& input) //creating and defining my Input function
+{
+    string result = ""; //creating string variable called result
+    for(char s : input) //loop that goes through each character in my input
+    {
+        if(isalpha(s)) //condition for checking if the character is an actual letter
+        {
+            result += toupper(s); //makes the letter uppercase
+        }
+    }
+    return result; //returns my new result after capitalizing each letter
+}
+string Encrypt(const string& phrase, int shift) //creating and defining my Encrypt function
 {
     string result = ""; //creating string variable called result
     for(char s : phrase) //loop that goes through each character in my text phrase
     {
-        if(isupper(s)) //checks for uppercase letters
-        {
-            result += char(int(s + shift - 'A') % 26 + 'A'); //shifts any uppercase characters and sets that as the result
-        }
-        else if(islower(s)) //checks for lowercase letters
-        {
-            result += char(int(s + shift - 'a') % 26 + 'a'); //shifts any lowercase characters and sets that as the result
-        }
-        else //if there's no letters to check
-        {
-            result += s; //keeps characters unchanged
-        }
+        result += char((s -'A' + shift) % 26 + 'A'); //shifts any uppercase characters and sets that as the result
     }
     return result; //returns my encrypted phrase
+}
+void Output(const string& encrypted)
+{
+  int count = 0; //creating count variable
+  for(char s : encrypted) //checks each letter in my encrypted phrase
+  {
+    cout << s; //printing each letter
+    count++; //increasing count
+    if(count % 5 == 0) 
+    {
+        cout << " "; //creates space after every 5 letters
+    }
+    if(count % 50 == 0) 
+    {
+        cout << endl; //creates new line after 10 blocks
+    }
+  }
+  if(count % 50 != 0) 
+  {
+    cout << endl; //creates last newline if necessary  
+  }
 }
 int main() //function compiler for my program
 {
@@ -29,7 +51,9 @@ int main() //function compiler for my program
     getline(cin, phrase); //takes the user input for phrase
     cout << "Enter shift: " << endl; //asks user to enter shift value
     cin >> shift; //takes the user input for shift
-    string encrypted = Encrypt(phrase, shift); //calling my encrypt function
-    cout << "Encrypted phrase: " << encrypted << endl; //prints my encrypted phrase
+    shift = (shift % 26 + 26) % 26; //keeps shift within range
+    string newInput = Input(phrase);
+    string encrypted = Encrypt(newInput, shift); //calling my encrypt function
+    Output(encrypted); //prints my encrypted phrase
     return 0; //ends function
 }
